b9969946ea0b0e618621302837d9de642041b646
Anthony Ruffalo <antruffii@gmail.com>
2021-07-29 13:44:16 -0500

    Update Formatting and Made buttons clickable

--- src/index.js
+++ src/index.js
@@ -3,23 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        value: null,
+      };
+    }
+
     render() {
       return (
-        <button className="square">
-          {this.props.value}
+        <button
+          className="square"
+          onClick={() => this.setState({value: 'X'})}
+        >
+          {this.state.value}
         </button>
       );
     }
   }
-  
+
   class Board extends React.Component {
     renderSquare(i) {
       return <Square value={i}/>;
     }
-  
+
     render() {
       const status = 'Next player: X';
-  
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -42,7 +52,7 @@ class Square extends React.Component {
       );
     }
   }
-  
+
   class Game extends React.Component {
     render() {
       return (
@@ -58,9 +68,9 @@ class Square extends React.Component {
       );
     }
   }
-  
+
   // ========================================
-  
+
   ReactDOM.render(
     <Game />,
     document.getElementById('root')
